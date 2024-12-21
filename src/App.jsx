e44712b9867c4a3c60dd2a7756da92669bd80603
Daniel Martinez <edu211004@gmail.com>
2024-12-21 16:04:34 -0600

    adding filter input

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,28 @@ import React from 'react'
 import { products } from './mocks/products.json'
 import './app.css'
 import { CartRemoveIcon } from './assets/svg'
-
+import Filters from './components/Filters'
 
 function App() {
-  const handleClick = ()=>{
-    
+  const handleClick = () => {
+
   }
   return (
-    <div className='appContainer'>
-      <h1>Shopping Cart 🛍  </h1>
-      <div className='productsContainer'>
-        {products.slice(0, 5).map(p => (
-          <div className='product' key={p.id}>
-            <img src={p.images[0]} alt="" />
-            <div>
-            <p>{p.title} <strong>{p.price}</strong> </p>
-            <button onClick={handleClick}><CartRemoveIcon/></button>
+    <div>
+      <Filters products={products}/>
+      <div className='appContainer'>
+        <h1>Shopping Cart 🛍  </h1>
+        <div className='productsContainer'>
+          {products.slice(0, 5).map(p => (
+            <div className='product' key={p.id}>
+              <img src={p.images[0]} alt="" />
+              <div>
+                <p>{p.title} <strong>{p.price}</strong> </p>
+                <button onClick={handleClick}><CartRemoveIcon /></button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   )
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useState } from 'react'
 
-function Filters() {
-  return (
-    <div>
-        <input type="range" name="" id="" />
-    </div>
-  )
+function Filters({products}) {
+    const [value, setValue] = useState('default')
+    const getCategories = () => {
+        const names = {}
+        products.forEach(p=>{
+            if(!names[p.category]){
+                names[p.category]= p.category
+            }
+        })
+        console.log(Object.keys(names))
+        
+    }
+    getCategories()
+    return (
+        <div>
+            <input type="range" name="" id="" min={0} max={1500} onChange={(e) => console.log(e.target.value)} />
+            <select name="" id="" value={value || 'default'} onChange={(e)=> console.log(e.target.value)}>
+                <option value="default">Select an option</option>
+                <option value="beauty">Beauty</option>
+            </select>
+        </div>
+    )
 }
 
 export default Filters
