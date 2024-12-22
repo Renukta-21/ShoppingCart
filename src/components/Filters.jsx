cadbdea99c291a7a350a6a580cf94246313eea37
Daniel Martinez <edu211004@gmail.com>
2024-12-21 19:10:52 -0600

    retrieving categories

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -9,16 +9,18 @@ function Filters({products}) {
                 names[p.category]= p.category
             }
         })
-        console.log(Object.keys(names))
-        
+        return Object.keys(names)
     }
-    getCategories()
+    console.log(getCategories())
+
     return (
         <div>
             <input type="range" name="" id="" min={0} max={1500} onChange={(e) => console.log(e.target.value)} />
             <select name="" id="" value={value || 'default'} onChange={(e)=> console.log(e.target.value)}>
                 <option value="default">Select an option</option>
-                <option value="beauty">Beauty</option>
+                {getCategories().map(c=>(
+                    <option value={c}>{c}</option>
+                ))}
             </select>
         </div>
     )
