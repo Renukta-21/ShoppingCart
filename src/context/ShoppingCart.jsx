1f2e6bf297e8043c61c87749f939eb43cc8e7ee3
Daniel Urbina <edu211004@gmail.com>
2024-12-23 22:22:59 -0600

    refactoring

--- src/context/ShoppingCart.jsx
+++ src/context/ShoppingCart.jsx
@@ -1,63 +1,10 @@
 import { createContext, useReducer, useState } from 'react'
-import { products } from '../mocks/products.json'
+import {cartReducer, cartInitialState} from '../reducers/cart'
 
 export const ShoppingCart = createContext()
 
-const initialState = {
-  products,
-  cart: [],
-  filters: { maxPrice: 0, category: 'all' },
-}
-
-const globalReducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TO_CART': {
-      const index = state.cart.findIndex((p) => p.id === action.payload.id)
-      if (index === -1) {
-        return {
-          ...state,
-          cart: [...state.cart, {...action.payload, quantity:1}]
-        }
-      }
-
-      const updatedCart = [...state.cart]
-      updatedCart[index].quantity +=1
-      return {
-        ...state,
-        cart:updatedCart
-      }
-    }
-
-    case 'REMOVE_ONE_FROM_CART':{
-      const productIdx = state.cart.findIndex(p=> p.id === action.payload.id)
-      const updatedCart = [...state.cart]
-      
-      if(updatedCart[productIdx].quantity > 1){
-        updatedCart[productIdx].quantity -=1
-      }else{
-        updatedCart.splice(productIdx, 1)
-      }
-      return {
-        ...state,
-        cart:updatedCart
-      } 
-    }
-
-    case 'REMOVE_FROM_CART':{
-      const productIdx = state.cart.findIndex(p=> p.id === action.payload.id)
-      const updatedCart = [...state.cart]
-      updatedCart.splice(productIdx, 1)
-
-      return {
-        ...state,
-        cart:updatedCart
-      } 
-    }
-  }
-}
-
 export const ShoppingCartProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(globalReducer, initialState)
+  const [state, dispatch] = useReducer(cartReducer, cartInitialState)
   const [filters, setFilters] = useState({
     maxPrice: 0,
     category: 'all',
---
+++ src/reducers/cart.jsx
@@ -0,0 +1,53 @@
+import { products } from '../mocks/products.json'
+
+export const cartReducer = (state, action) => {
+    switch (action.type) {
+      case 'ADD_TO_CART': {
+        const index = state.cart.findIndex((p) => p.id === action.payload.id)
+        if (index === -1) {
+          return {
+            ...state,
+            cart: [...state.cart, {...action.payload, quantity:1}]
+          }
+        }
+  
+        const updatedCart = [...state.cart]
+        updatedCart[index].quantity +=1
+        return {
+          ...state,
+          cart:updatedCart
+        }
+      }
+  
+      case 'REMOVE_ONE_FROM_CART':{
+        const productIdx = state.cart.findIndex(p=> p.id === action.payload.id)
+        const updatedCart = [...state.cart]
+        
+        if(updatedCart[productIdx].quantity > 1){
+          updatedCart[productIdx].quantity -=1
+        }else{
+          updatedCart.splice(productIdx, 1)
+        }
+        return {
+          ...state,
+          cart:updatedCart
+        } 
+      }
+  
+      case 'REMOVE_FROM_CART':{
+        const productIdx = state.cart.findIndex(p=> p.id === action.payload.id)
+        const updatedCart = [...state.cart]
+        updatedCart.splice(productIdx, 1)
+  
+        return {
+          ...state,
+          cart:updatedCart
+        } 
+      }
+    }
+  }
+  export const cartInitialState = {
+    products,
+    cart: [],
+    filters: { maxPrice: 0, category: 'all' },
+  }
